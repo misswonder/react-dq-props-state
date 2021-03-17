ff451c77030b3b854b0aac41054b9b17e74bc31b
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-17 12:57:41 -0500

    done

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -12,6 +12,9 @@ export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 0,
+    }
   }
 
   changeWeight = (e) => {
@@ -25,20 +28,24 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
+        <h1>{this.props.name}</h1>
+        <h3>Weight: {this.state.weight} </h3>
+        <h3>Hobby: {this.props.hobby} </h3>
+        <h4>Eye Color: {this.props.eyeColor} </h4>
           
-        <Button name="+">
+        <Button onClick={(e) => this.changeWeight(e)} name="+">
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button onClick={(e) => this.changeWeight(e)} name="-">
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+        <img
+            src={normalBaby}
+            style={{height: "200px"}}
+            alt="MasterBlasterJrJr"
+          />
         </div>
         
       </li>
